1d7ad08578b8c8a45f689c3a51186fb96e70702e
taskdire <136046239+taskdire@users.noreply.github.com>
2024-11-04 12:28:48 -0500

    awda
    awdawd

---
+++ app/dom.js
@@ -0,0 +1,5 @@
+const Domselectors = {
+  box: document.getElementById("container"),
+};
+
+export { Domselectors };
--- app/main.js
+++ app/main.js
@@ -1,8 +1,20 @@
-import "./style.css";
-import { Food } from "./products";
+import "../css/style.css";
+import { menuItems as Food } from "./products";
+import { Domselectors } from "./dom";
 
-const Foodcontainer = document.getElementById("Foodcontainer");
+function Cards() {
+  Food.forEach((menuItem) => {
+    Domselectors.box.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <h2>${menuItem.title}</h2>
+        <img src="${menuItem.image}" alt="" class="card-img">
+        <h2>${menuItem.vegan}</h2>
+        <h3>${menuItem.type}</h3>
+        <p>$${menuItem.price}<p>
+      </div>`
+    );
+  });
+}
 
-Food.array.forEach((card) => {
-  const card = <div class="card"></div>;
-});
+Cards();
