dc4c65939cc723f63f0ca9a3c37c142d763ff278
taskdire <136046239+taskdire@users.noreply.github.com>
2024-11-06 12:29:04 -0500

    awd
    awd

--- app/main.js
+++ app/main.js
@@ -1,17 +1,17 @@
-import "../css/style.css";
-import { menuItems as Food } from "./products";
+import "/style.css";
+import { menuItems } from "./products";
 import { Domselectors } from "./dom";
 
 function Cards() {
-  Food.forEach((menuItem) => {
+  menuItems.forEach((menuItems) => {
     Domselectors.box.insertAdjacentHTML(
       "beforeend",
       `<div class="card">
-        <h2>${menuItem.title}</h2>
-        <img src="${menuItem.image}" alt="" class="card-img">
-        <h2>${menuItem.vegan}</h2>
-        <h3>${menuItem.type}</h3>
-        <p>$${menuItem.price}<p>
+        <h2>${menuItems.title}</h2>
+        <img src="${menuItems.image}" alt="" class="card-img">
+        <h2>${menuItems.vegan}</h2>
+        <h3>${menuItems.type}</h3>
+        <p>$${menuItems.price}<p>
       </div>`
     );
   });
